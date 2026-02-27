8c156e2563ccc84d32d1524af506c06beb3a05b7
dilorazamova <dildorazamov9@gmail.com>
2026-02-28 00:16:51 +0500

    prepare backend for production

--- hakan_travel/settings.py
+++ hakan_travel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ro$cjoi&#admn*7nauf+%+=06eyxm0u*9$4_=(qf7u)4ee2vez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'hakan_travel.urls'
