abd7b965944afe14846fc23bc0171db16d689d59
dilorazamova <dildorazamov9@gmail.com>
2026-02-27 23:42:23 +0500

    update allowed hosts

--- hakan_travel/settings.py
+++ hakan_travel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ro$cjoi&#admn*7nauf+%+=06eyxm0u*9$4_=(qf7u)4ee2vez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
